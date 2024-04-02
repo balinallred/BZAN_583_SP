c63e96993f30a0270d92133a8f87acd335d20bb4
Balin Allred <balindavidallred@gmail.com>
2024-04-02 11:25:37 -0400

    Original

--- profile.R
+++ profile.R
@@ -1,7 +1,34 @@
-x = matrix(runif(1e5*500), ncol = 500)
-
-
 Rprof()
-x_pr = prcomp(x, retx = FALSE)
+
+start <- Sys.time()
+
+n <- 1000000
+counter <- 0
+
+for (i in 1:n) {
+  #initial wins
+  cubs <- 11
+  brewers <- 12
+  cardinals <- 15
+  
+  #cubs vs cards 6 games
+  cubs_wins <- sum(sample(c(0,1),6, replace = T))
+  cubs <- cubs + cubs_wins
+  cardinals <- cardinals + 6 - cubs_wins
+  
+  #cubs other 3 games
+  cubs <- cubs + sum(sample(c(0,1),3, replace = T))
+  
+  #brewers other 9 games
+  brewers <- brewers + sum(sample(c(0,1),9, replace = T))
+  
+  counter <- counter + ifelse(cubs == cardinals & cubs == brewers,1,0)
+  
+}
+
+print(counter/n)
+
+print(Sys.time() - start)
+
 Rprof(NULL)
 summaryRprof()
